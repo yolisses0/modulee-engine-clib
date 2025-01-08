8dc26b8f09157aab47bbe20d752f4733ad487d59
Yolisses <yolisses0@gmail.com>
2025-01-08 19:21:58 -0300

    Create graph create and destroy functions

--- src/graph.rs
+++ src/graph.rs
@@ -7,4 +7,21 @@ impl Graph {
     pub extern "C" fn get_debug_value(&self) -> f64 {
         self.graph.get_debug_value()
     }
+
+    fn new() -> Self {
+        Self {
+            graph: modulee_engine::graph::Graph::new(),
+        }
+    }
+
+    #[no_mangle]
+    pub extern "C" fn create_graph_pointer() -> *mut Self {
+        Box::into_raw(Box::new(Self::new()))
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn destroy_graph_pointer(ptr: *mut Self) {
+        assert!(!ptr.is_null());
+        drop(Box::from_raw(ptr));
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod graph;
 
-use modulee_engine::graph::Graph;
+extern crate modulee_engine;
+pub use graph::Graph;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
@@ -14,11 +15,5 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn some_import() {
-        let graph = Graph::new();
-        print!("{:?}", graph);
     }
 }
