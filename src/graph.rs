9e5df2bcf0b9d898726a37963edddf3da2169c57
Yolisses <yolisses0@gmail.com>
2025-01-13 06:38:35 -0300

    Create Graph::set_nodes

--- src/graph.rs
+++ src/graph.rs
@@ -22,6 +22,13 @@ impl Graph {
         println!("string value: {}", string_value);
     }
 
+    #[no_mangle]
+    pub extern "C" fn set_nodes(&mut self, nodes_data: *const c_char) {
+        let c_str = unsafe { CStr::from_ptr(nodes_data) };
+        let debug_string = c_str.to_str().expect("Bad encoding");
+        self.graph.set_nodes_from_json(debug_string);
+    }
+
     fn new() -> Self {
         Self {
             graph: modulee_engine::graph::Graph::new(),
