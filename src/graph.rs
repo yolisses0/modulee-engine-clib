95642fde6c491d2d7ef82c9dfa051b9221b489c9
Yolisses <yolisses0@gmail.com>
2025-05-25 08:37:52 -0300

    Format Graph

--- src/graph.rs
+++ src/graph.rs
@@ -26,6 +26,15 @@ impl Graph {
     }
 
     #[no_mangle]
+    pub extern "C" fn set_graph(&mut self, graph_data: *const c_char) {
+        let c_str = unsafe { CStr::from_ptr(graph_data) };
+        let graph_json = c_str.to_str().expect("Bad encoding");
+        if let Err(e) = self.graph.update_from_json(graph_json) {
+            eprintln!("Failed to set graph from JSON: {}\n{}", e, graph_json);
+        }
+    }
+
+    #[no_mangle]
     pub extern "C" fn process_block(&mut self, buffer: *mut f32, length: usize) {
         let buffer = unsafe { slice::from_raw_parts_mut(buffer, length) };
         self.graph.process_block(buffer, length);
@@ -39,14 +48,5 @@ impl Graph {
     #[no_mangle]
     pub extern "C" fn set_note_off(&mut self, pitch: f32) {
         self.graph.set_note_off(pitch);
-    }
-
-    #[no_mangle]
-    pub extern "C" fn set_graph(&mut self, graph_data: *const c_char) {
-        let c_str = unsafe { CStr::from_ptr(graph_data) };
-        let graph_json = c_str.to_str().expect("Bad encoding");
-        if let Err(e) = self.graph.update_from_json(graph_json) {
-            eprintln!("Failed to set graph from JSON: {}\n{}", e, graph_json);
-        }
     }
 }
